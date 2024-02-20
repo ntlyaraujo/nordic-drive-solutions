66a696afbfe858b3760ed34f1477f737350323eb
Nataly Araujo <nataly.araujo@logikfabriken.se>
2024-02-20 12:43:55 +0100

    Remove comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,6 @@ import type { AppProps } from "next/app";
 import React from "react";
 import Head from "next/head";
 
-/*
-Author: Nataly Christine Araujo
-Company: Logikfabriken
-*/
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
